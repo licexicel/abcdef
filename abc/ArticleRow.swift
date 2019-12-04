4188a83af1b214765e817871d9dca0ea61d9826c
XIE <x1998629x@gmail.com>
2019-12-04 01:17:35 -0800

    alahuagua

---
+++ abc/ArticleRow.swift
@@ -0,0 +1,31 @@
+//
+//  ArticleRow.swift
+//  abc
+//
+//  Created by XIE on 2019/12/4.
+//  Copyright © 2019 XIE. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct ArticleRow: View {
+    
+    var body: some View {
+        HStack {
+            Text("alahuagua")
+        }
+    }
+}
+
+struct ArticleRow_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        ArticleRow()
+    }
+}
+
+func testa() -> Void{
+    let tempa = shit_stuff()
+    tempa.Request_start()
+}
